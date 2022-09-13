ca0d8e4ba792479afd6879a405ff975b7afc5d98
krekiehn <nicolai.krekiehn@googlemail.com>
2022-09-13 23:26:02 +0200

    make simplenet even more simple due to OOM

--- SimpelNet.py
+++ SimpelNet.py
@@ -4,21 +4,21 @@ import tensorflow as tf
 def FCN_model(len_classes=5, dropout_rate=0.2, shape=(None, None, None, 1)):
     input = tf.keras.layers.Input(shape=shape)
 
-    x = tf.keras.layers.Conv3D(filters=32, kernel_size=3, strides=1, padding='same')(input)
+    x = tf.keras.layers.Conv3D(filters=16, kernel_size=3, strides=1, padding='same')(input)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
     # x = tf.keras.layers.MaxPooling3D()(x)
 
-    x = tf.keras.layers.Conv3D(filters=64, kernel_size=3, strides=1, padding='same')(x)
+    x = tf.keras.layers.Conv3D(filters=32, kernel_size=3, strides=1, padding='same')(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
     # x = tf.keras.layers.MaxPooling3D()(x)
 
-    x = tf.keras.layers.Conv3D(filters=128, kernel_size=3, strides=1)(x)
+    x = tf.keras.layers.Conv3D(filters=64, kernel_size=3, strides=1)(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
