bfb9f8f399a2d003a211fbb06b7cbdce62edcc14
krekiehn <nicolai.krekiehn@googlemail.com>
2022-09-13 23:05:01 +0200

    removed Conv3D block with 512 kernels to avoid OOM

--- SimpelNet.py
+++ SimpelNet.py
@@ -32,10 +32,10 @@ def FCN_model(len_classes=5, dropout_rate=0.2, shape=(None, None, None, 1)):
 
     # x = tf.keras.layers.MaxPooling3D()(x)
 
-    x = tf.keras.layers.Conv3D(filters=512, kernel_size=3, strides=1)(x)
-    x = tf.keras.layers.Dropout(dropout_rate)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Activation('relu')(x)
+    # x = tf.keras.layers.Conv3D(filters=512, kernel_size=3, strides=1)(x)
+    # x = tf.keras.layers.Dropout(dropout_rate)(x)
+    # x = tf.keras.layers.BatchNormalization()(x)
+    # x = tf.keras.layers.Activation('relu')(x)
 
     # Uncomment the below line if you're using dense layers
     # x = tf.keras.layers.GlobalMaxPooling3D()(x)
