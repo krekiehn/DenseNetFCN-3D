60196efef17f3be703ad539cb5d541194835c337
krekiehn <nicolai.krekiehn@googlemail.com>
2022-09-15 15:42:48 +0200

    due to OOM we need to reduced simpelnetwork

--- SimpelNet.py
+++ SimpelNet.py
@@ -6,21 +6,21 @@ def FCN_model(len_classes=5, dropout_rate=0.2, shape=(None, None, None, 1)):
 
     x = tf.keras.layers.GaussianNoise(stddev=1.0)(input)
 
-    x = tf.keras.layers.Conv3D(filters=32, kernel_size=3, strides=1, padding='same')(x)
+    x = tf.keras.layers.Conv3D(filters=16, kernel_size=3, strides=1, padding='same')(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
     x = tf.keras.layers.MaxPooling3D()(x)
 
-    x = tf.keras.layers.Conv3D(filters=64, kernel_size=3, strides=1, padding='same')(x)
+    x = tf.keras.layers.Conv3D(filters=16*8, kernel_size=3, strides=1, padding='same')(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
 
     x = tf.keras.layers.MaxPooling3D()(x)
 
-    x = tf.keras.layers.Conv3D(filters=128, kernel_size=3, strides=1)(x)
+    x = tf.keras.layers.Conv3D(filters=16*8*8, kernel_size=3, strides=1)(x)
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.Activation('relu')(x)
