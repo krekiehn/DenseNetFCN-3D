11921cfbf183f2f290e734f96d245e93792af97b
krekiehn <nicolai.krekiehn@googlemail.com>
2022-09-16 14:07:02 +0200

    implement to load latest model weights

--- seg_vertebra_instance_qa/train.py
+++ seg_vertebra_instance_qa/train.py
@@ -35,6 +35,8 @@ def train(model, train_generator, val_generator, epochs=50):
     # search_dir = "/mydir/"
     # os.chdir(checkpoint_path)
     print(os.listdir())
+    print(checkpoint_path,os.path.isdir(checkpoint_path))
+    print(checkpoint_path_2, os.path.isdir(checkpoint_path_2))
     files = filter(os.path.isfile, os.listdir(checkpoint_path_2))
     files = [os.path.join(checkpoint_path_2, f) for f in files]  # add path to each file
     files.sort(key=lambda x: os.path.getmtime(x))
