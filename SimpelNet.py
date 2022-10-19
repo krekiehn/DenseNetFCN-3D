981d816dea8c98c86eff1bb720ec810ece70b8a0
krekiehn <nicolai.krekiehn@googlemail.com>
2022-10-19 22:10:13 +0200

    mixed precision on

--- SimpelNet.py
+++ SimpelNet.py
@@ -65,7 +65,7 @@ def FCN_model(len_classes=5, dropout_rate=0.2, shape=(None, None, None, 1)):
     x = tf.keras.layers.Dropout(dropout_rate)(x)
     x = tf.keras.layers.BatchNormalization()(x)
     x = tf.keras.layers.GlobalMaxPooling3D()(x)
-    predictions = tf.keras.layers.Activation('softmax')(x)
+    predictions = tf.keras.layers.Activation('softmax', dtype='float32', name='predictions')(x)
 
     model = tf.keras.Model(inputs=input, outputs=predictions)
 
