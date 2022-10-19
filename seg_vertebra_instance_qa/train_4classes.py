96fc2f1af7efdfa64e8a9dfc7c193b42f6050dba
krekiehn <nicolai.krekiehn@googlemail.com>
2022-10-19 21:27:31 +0200

    acutaly enable the training :D

--- seg_vertebra_instance_qa/train_4classes.py
+++ seg_vertebra_instance_qa/train_4classes.py
@@ -61,7 +61,8 @@ if __name__ == '__main__':
     # model_fcn.summary()
 
     # multi gpu
-    if multi_gpu_flag:
+    # if multi_gpu_flag:
+    if False:
         mirrored_strategy = tf.distribute.MirroredStrategy()
 
         with mirrored_strategy.scope():
