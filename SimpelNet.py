68fcf917fa225d9e5a3233d8912d5bdbd28620de
nicolai <nicolai.krekiehn@rad.uni-kiel.de>
2022-11-17 16:20:18 +0100

    remove one conv block

--- SimpelNet.py
+++ SimpelNet.py
@@ -36,10 +36,10 @@ def FCN_model(len_classes=5, dropout_rate=0.2, shape=(None, None, None, 1)):
 
     x = tf.keras.layers.MaxPooling3D()(x)
 
-    x = tf.keras.layers.Conv3D(filters=16*8*8, kernel_size=1, strides=1)(x)
-    #x = tf.keras.layers.Dropout(dropout_rate)(x)
-    x = tf.keras.layers.BatchNormalization()(x)
-    x = tf.keras.layers.Activation('relu')(x)
+    # x = tf.keras.layers.Conv3D(filters=16*8*8, kernel_size=1, strides=1)(x)
+    # #x = tf.keras.layers.Dropout(dropout_rate)(x)
+    # x = tf.keras.layers.BatchNormalization()(x)
+    # x = tf.keras.layers.Activation('relu')(x)
 
     # Uncomment the below line if you're using dense layers
 
